2b6a430160bb7b32e12e47c3d8060cb8e5a199d0
JChristianRojo <jchristianrojo@gmail.com>
2024-05-21 18:15:25 +0800

    add promise code sample

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ function addToDo() {
 
     setTimeout(clearInputField, 500);
 }
+
+
+// function clearInputField() {
+//     document.getElementById("to-do-input").value = "";
+// }
+
+// const addToDoPromise = new Promise((resolve, reject) => {
+//     const li = document.createElement("li");
+//     const toDoName = document.getElementById("to-do-input").value;
+//     const liText = document.createTextNode(toDoName);
+
+//     li.appendChild(liText);
+
+//     if (toDoName === "") {
+//         reject("This cannot be empty");
+//     } else {
+//         document.getElementById("to-do-list").appendChild(li);
+//         resolve(toDoName);
+//     }
+// });
+
+// addToDoPromise.then((toDoName) => {
+//     console.log("Added ToDo: " + toDoName);
+//     clearInputField();
+// }).catch((error) => {
+//     console.log(error);
+// });
